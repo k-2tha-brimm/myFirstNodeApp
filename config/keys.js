3f97e604cf020a5eb6d28d98e31501fd0375eac1
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-11 12:40:31 -0700

    remove keys.js from git

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://developer:I1JubCD0iyG0Dazc@mongodb-5ffgt.mongodb.net/test?retryWrites=true'
-    //fill this in with my string once I have access to it
-}
