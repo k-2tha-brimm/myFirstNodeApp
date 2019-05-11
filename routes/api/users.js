fec45ddf099bc146f951cf802ea3aff5579e6269
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-11 15:23:08 -0700

    validate register

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,18 @@ const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    
     User.findOne( { email: req.body.email } )
     .then(user => {
         if(user) {
