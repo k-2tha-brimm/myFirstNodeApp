efeb58cd56fb5e4360730e96d7700a818a321463
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-11 12:36:52 -0700

    add git ignore file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose")
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello, ya bish"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
